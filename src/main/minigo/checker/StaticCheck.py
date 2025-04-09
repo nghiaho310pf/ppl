62bbe4789ae531d285577f458dd42de14ac320fe
nghiaho310pf <>
2025-04-09 19:31:25 +0700

    lol

--- src/main/minigo/checker/StaticCheck.py
+++ src/main/minigo/checker/StaticCheck.py
@@ -905,7 +905,6 @@ class StaticChecker(BaseVisitor):
         if isinstance(implicit_type, AST.VoidType):
             raise StaticError.TypeMismatch(ast.iniExpr)
         if (explicit_type is not None) and (implicit_type is not None) and (not self.can_cast_a_to_b(implicit_type, explicit_type)):
-            # TODO: ask prof. Phung why we have to pass ast instead of ast.varInit. Cite "Test Before Submitting".
             raise StaticError.TypeMismatch(ast)
 
         if (explicit_type is None) and isinstance(implicit_type, NilType):
@@ -1106,7 +1105,6 @@ class StaticChecker(BaseVisitor):
 
         condition_type = self.visit(ast.cond, my_scope + [IsExpressionVisit()])
         if not isinstance(condition_type, AST.BoolType):
-            # TODO: Ask prof. Phung whether to pass ast or ast.expr.
             raise StaticError.TypeMismatch(ast)
 
         if isinstance(ast.upda.lhs, AST.Id):
